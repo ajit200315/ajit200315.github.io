9a8f523007139dae038af16133f2523d44193ebe
Ajitsingh Arjunsingh Chauhan <113981559+ajit200315@users.noreply.github.com>
2025-06-19 21:47:29 -0700

    chore: deploy React/Vite build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',  
   plugins: [tailwindcss(),react()],
+  build: {
+    outDir: 'docs',       // <-- output to docs/ instead of dist/
+  },
 })
